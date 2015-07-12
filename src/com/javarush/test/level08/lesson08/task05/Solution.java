18294f254d0933f6ccfd18cb0f5cb4366f910539
vitaliyvorona <voronchik84@gmail.com>
2015-07-12 18:25:59 +0300

    Map - delete the same values

--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.test.level08.lesson08.task05;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /* Удалить людей, имеющих одинаковые имена
 Создать словарь (Map<String, String>) занести в него десять записей по принципу «фамилия» - «имя».
 Удалить людей, имеющих одинаковые имена.
