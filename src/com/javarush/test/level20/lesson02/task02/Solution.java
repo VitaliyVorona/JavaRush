15105f1e3b818c6bd90cf11f4170c64c9d2d4866
Vitaliy Vorona <vitaliy.vorona@avid.com>
2015-11-02 14:49:04 +0200

    task 2 modified

--- src/com/javarush/test/level20/lesson02/task02/Solution.java
+++ src/com/javarush/test/level20/lesson02/task02/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level20.lesson02.task02;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /* Читаем и пишем в файл: JavaRush
@@ -14,7 +15,7 @@ public class Solution {
         //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
-            File your_file_name = File.createTempFile("your_file_name", null);
+            File your_file_name = File.createTempFile("your_file_name", ".txt");
             OutputStream outputStream = new FileOutputStream(your_file_name);
             InputStream inputStream = new FileInputStream(your_file_name);
 
@@ -23,15 +24,17 @@ public class Solution {
             User user = new User();
             user.setFirstName("Alex");
             user.setLastName("Ivanov");
-            user.setCountry(User.Country.UKRAINE);
+//            user.setCountry(User.Country.UKRAINE);
             user.getBirthDate();
-            javaRush.users.add(new User());
+            javaRush.users.add(user);
             javaRush.save(outputStream);
             outputStream.flush();
+
 
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
             //check here that javaRush object equals to loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            System.out.println(javaRush.equals(loadedObject));
 
             outputStream.close();
             inputStream.close();
@@ -51,18 +54,110 @@ public class Solution {
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
             PrintWriter writer = new PrintWriter(outputStream);
-            int usersCounter = 0;
 
             String isUserPresent = users != null ? "yes" : "no";
             writer.println(isUserPresent);
 
             int usersNumber = users.size();
-            for (int i=0; i)
-            writer.println();
+            writer.println(usersNumber);
+
+            for (int i = 0; i < usersNumber; i++) {
+                User user = users.get(i);
+                if (user.getFirstName() != null) {
+                    writer.println(user.getFirstName());
+                } else writer.println("null");
+
+                if (user.getLastName() != null) {
+                    writer.println(user.getLastName());
+                } else writer.println("null");
+
+                if (user.getBirthDate() != null) {
+                    writer.println(user.getBirthDate());
+                } else writer.println("null");
+
+                writer.println(user.isMale());
+
+                if (user.getCountry() != null) {
+                    writer.println(user.getCountry());
+                } else writer.println("null");
+            }
+            writer.println("End of Users");
+            writer.flush();
+            writer.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String isUserPresent = reader.readLine();
+            String isEndOfUsers = "End of Users";
+
+            if (isUserPresent.equals("yes")) {
+                String line = reader.readLine();
+                int usersNumber = Integer.parseInt(line);
+
+                while (!line.equals(isEndOfUsers)) {
+
+                    for (int i = 0; i < usersNumber; i++) {
+                        this.users.add(i, new User());
+
+                        line = reader.readLine();
+                        if (!line.equals("null")) {
+                            this.users.get(i).setFirstName(line);
+                        } else this.users.get(i).setFirstName("null");
+
+                        line = reader.readLine();
+                        if (!line.equals("null")) {
+                            this.users.get(i).setLastName(line);
+                        } else this.users.get(i).setLastName("null");
+
+                        line = reader.readLine();
+                        if (!line.equals("null")) {
+                            this.users.get(i).setBirthDate(new Date(line));
+                        } else this.users.get(i).setBirthDate(null);
+
+                        line = reader.readLine();
+                        if (line.equals(true)) {
+                            this.users.get(i).setMale(true);
+                        } else this.users.get(i).setMale(false);
+
+                        line = reader.readLine();
+                        if (!line.equals(null) && line.equals("UKRAINE")) {
+                            this.users.get(i).setCountry(User.Country.UKRAINE);
+                        }
+                        if (!line.equals(null) && line.equals("RUSSIA")) {
+                            this.users.get(i).setCountry(User.Country.RUSSIA);
+                        }
+                        if (!line.equals(null) && line.equals("OTHER")) {
+                            this.users.get(i).setCountry(User.Country.OTHER);
+                        }
+                        if (line.equals(null) || line.equals("null")) {
+                            this.users.get(i).setCountry(null);
+                        }
+                    }
+                    line = reader.readLine();
+                }
+            }
+            reader.close();
+        }
+
+        public boolean equals(JavaRush rush) {
+            boolean result = false;
+            if (rush != null) {
+                if (rush.users != null) {
+                    int usersNum = users.size();
+                    if (this.users.size() == rush.users.size()) {
+                        for (int i = 0; i < usersNum; i++) {
+                                result = this.users.get(i).getFirstName().equals(rush.users.get(i).getFirstName()) &&
+                                        this.users.get(i).getLastName().equals(rush.users.get(i).getLastName()) &&
+                                        this.users.get(i).getBirthDate() == rush.users.get(i).getBirthDate() &&
+                                        this.users.get(i).isMale() == rush.users.get(i).isMale() &&
+                                        this.users.get(i).getCountry() == rush.users.get(i).getCountry();
+                        }
+                    }
+                }
+            }
+            return result;
         }
     }
 }
